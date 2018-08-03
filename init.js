8d0844294f615a636a4a4e87302366528379f886
afiq rostam <afiqrostam@hotmail.com>
2018-08-03 12:15:26 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,2 +1,13 @@
 var init;
-google.script.run.withSuccessHandler(function(e){if(e.status){init=e.content}}).get_var('init')
+$('#main').hide();
+$('#feed').html(get_alert({id:'feed_load',type:'secondary',msg:'loading data.'}));
+google.script.run.withSuccessHandler(
+  function(e){
+    $('#feed_load').alert('close');
+    if(e.status){
+      $('#feed').html(get_alert({id:'feed_success',type:'primary',msg:'load data success.'}));
+      setTimeout(function(){
+        $('#feed_success').alert('close');
+        init=e.content;
+        $('#main').show()},1000,e)}
+    else{$('#feed').html(get_alert({id:'feed_fail',type:'danger',msg:'load data fail.'}))}}).get_var('init')
