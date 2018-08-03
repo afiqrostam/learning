36d2945443e0bce35f225bb38c3c957796f46e9a
afiq rostam <afiqrostam@hotmail.com>
2018-08-03 15:35:06 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,3 +1,8 @@
 var get_alert=function(e){
-return $('<div id="'+e.id+'" class="alert alert-'+e.type+' alert-dismissible fade show" role="alert">').append(
-  $('<strong>').html('!!! ')).append(e.msg)}
+  if(e!=undefined){
+    var alert=$('<div id="'+e.id+'" class="alert alert-'+e.type+' alert-dismissible fade show" role="alert">').append(
+      $('<strong>').html('!!! ')).append(e.msg);
+    if(e.feed){
+      $('#feed').append(alert);
+      setTimeout(function(){alert.alert('close')},1500,alert)}
+    else{return alert}}}
