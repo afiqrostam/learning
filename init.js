b9bbec37fd6fb468c7323d3c1393d44ab4518cc5
afiq rostam <afiqrostam@hotmail.com>
2018-08-03 15:41:15 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,12 +1,9 @@
 var init;
-$('#feed').html(get_alert({id:'feed_load',type:'secondary',msg:'loading data.'}));
+get_alert({id:'feed_load',type:'secondary',msg:'loading data.',feed:true});
 google.script.run.withSuccessHandler(
   function(e){
-    $('#feed_load').alert('close');
     if(e.status){
-      $('#feed').append(get_alert({id:'feed_success',type:'primary',msg:'load data success.'}));
-      setTimeout(function(){
-        $('#feed_success').alert('close');
-        init=e.content;
-        $('#main').show()},500,e)}
-    else{$('#feed').append(get_alert({id:'feed_fail',type:'danger',msg:'load data fail.'}))}}).get_var('init')
+      get_alert({id:'feed_success',type:'primary',msg:'load data success.',feed:true});
+      init=e.content;
+      $('#main').show()}
+    else{get_alert({id:'feed_fail',type:'danger',msg:'load data fail.',feed:true})}}).get_var('init')
