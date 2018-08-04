9940568c9b6ea64e0a611bd50faf94f6b4e6705c
afiq rostam <afiqrostam@hotmail.com>
2018-08-04 19:18:08 +0800

    Update init.js

--- script/init.js
+++ script/init.js
@@ -1,10 +1,16 @@
 var init;
 get_alert({id:'feed_load',type:'secondary',msg:'loading data.',feed:true});
-google.script.run.withSuccessHandler(
-  function(e){
-    if(e.status){
-      get_alert({id:'feed_success',type:'primary',msg:'load data success.',feed:true});
-      $('#feed_success').on('closed.bs.alert',function(){
-        init=e.content;
-        $('#main').addClass('show')})}
-    else{get_alert({id:'feed_fail',type:'danger',msg:'load data fail.',feed:true})}}).get_var('init')
+if(window.location.host.includes('googleusercontent')){
+  google.script.run.withSuccessHandler(
+    function(e){
+      if(e.status){
+        get_alert({id:'feed_success',type:'primary',msg:'load data success.',feed:true});
+        $('#feed_success').on('closed.bs.alert',function(){
+          init=e.content;
+          $('#main').addClass('show')})}
+      else{
+        get_alert({id:'feed_fail',type:'danger',msg:'load data fail.',feed:true});
+        $('#main').addClass('show')}}).get_var('init')}
+else{
+  get_alert({id:'feed_fail',type:'danger',msg:'load data fail.',feed:true});
+  $('#main').addClass('show')}
