3028fa1478168a75e2ecdf321e32825291ec36bd
afiq rostam <afiqrostam@hotmail.com>
2018-08-05 18:32:58 +0800

    Update init.js

--- script/init.js
+++ script/init.js
@@ -1,17 +1,20 @@
 var init;
 var def={};
-get_alert({id:'feed_load',type:'secondary',msg:'loading data.',feed:true});
+get_alert({id:'feed_load',type:'secondary',msg:'initialising..',feed:true});
 if(window.location.host.includes('googleusercontent')){
   google.script.run.withSuccessHandler(
     function(e){
-      if(e.status){
-        get_alert({id:'feed_success',type:'primary',msg:'load data success.',feed:true});
+      if(e.s){
+        get_alert({id:'feed_success',type:'primary',msg:'initialising..success',feed:true});
         $('#feed_success').on('closed.bs.alert',function(){
-          init=e.content;
-          $('#main').addClass('show')})}
+          init=e.res;
+          $('#main').addClass('show');
+          get_alert({id:'feed_load',type:'secondary',msg:'load..data',feed:true});
+          get_ready();
+        })}
       else{
-        get_alert({id:'feed_fail',type:'danger',msg:'load data fail.',feed:true});
+        get_alert({id:'feed_fail',type:'danger',msg:'initialising..fail',feed:true});
         $('#feed_fail').on('closed.bs.alert',function(){$('#main').addClass('show')})}}).get_var('init')}
 else{
-  get_alert({id:'feed_fail',type:'danger',msg:'load data fail.',feed:true});
+  get_alert({id:'feed_fail',type:'danger',msg:'initialising..fail',feed:true});
   $('#feed_fail').on('closed.bs.alert',function(){$('#main').addClass('show')})}
