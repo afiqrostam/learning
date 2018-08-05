456d4f01495096b5f079474d90aeed4efb539a08
afiq rostam <afiqrostam@hotmail.com>
2018-08-06 00:23:34 +0800

    Update db.js

--- script/db.js
+++ script/db.js
@@ -17,21 +17,39 @@ function get_bu_child(id){
     var find_bu=Object.getOwnPropertyNames(def.bu).filter(
       function(e){return def.bu[e].root==id}).map(
         function(e){return def.bu[e]});
-    if(find_bu.length!=0){
+    var find_project=Object.getOwnPropertyNames(def.project).filter(
+      function(e){return def.project[e].root.split(',').indexOf(id)!=-1}).map(
+        function(e){return def.project[e]});
+    if(find_bu.length!=0&&find_project.length!=0){
+      console.timeEnd(arguments.callee.name);
+      return find_bu.concat(find_project)}
+    else if(find_bu.length!=0){
       console.timeEnd(arguments.callee.name);
       return find_bu}
+    else if(find_project.length!=0){
+      console.timeEnd(arguments.callee.name);
+      return find_project}
     else{
-  	  var find_project=Object.getOwnPropertyNames(def.project).filter(
-        function(e){return def.project[e].root.split(',').indexOf(id)!=-1}).map(
-          function(e){return def.project[e]});
-  	  if(find_project.length!=0){
-        console.timeEnd(arguments.callee.name);
-        return find_project}
-  	  else{
-        console.error('no futher child found');
-        console.timeEnd(arguments.callee.name);
-        return false}}}}
-      
+      console.error('no futher child found');
+      console.timeEnd(arguments.callee.name);
+      return false}}}
+         
+function get_country_current(){
+  return array_unique(Object.getOwnPropertyNames(def.project).map(
+    function(e){return def.project[e].country})).map(
+      function(e){return def.country[e]})}
+
+function get_country_child(id){
+  console.time(arguments.callee.name);
+  if(id==undefined){console.error('ID not defined');console.timeEnd(arguments.callee.name);return false}
+  else if(id.substr(0,1)!='C'){console.error('invalid ID type');console.timeEnd(arguments.callee.name);return false}
+  else{
+    var find_project=Object.getOwnPropertyNames(def.project).filter(
+          function(e){return def.project[e].country==id}).map(
+              function(e){return def.project[e]});
+    if(find_project.length!=0){console.timeEnd(arguments.callee.name);return find_project}
+    else{console.error('no futher child found');console.timeEnd(arguments.callee.name);return false}}}
+
 function get_project_country(id){
   console.time(arguments.callee.name);
   if(id==undefined){
@@ -72,19 +90,3 @@ else{
       def.project[id].country=trim[0];
       console.timeEnd(arguments.callee.name);
       return def.project[id].country}}}}
-      
-function get_country_child(id){
-  console.time(arguments.callee.name);
-  if(id==undefined){console.error('ID not defined');console.timeEnd(arguments.callee.name);return false}
-  else if(id.substr(0,1)!='C'){console.error('invalid ID type');console.timeEnd(arguments.callee.name);return false}
-  else{
-    var find_project=Object.getOwnPropertyNames(def.project).filter(
-          function(e){return def.project[e].country==id}).map(
-              function(e){return def.project[e]});
-    if(find_project.length!=0){console.timeEnd(arguments.callee.name);return find_project}
-    else{console.error('no futher child found');console.timeEnd(arguments.callee.name);return false}}}
-    
-function get_country_current(){
-  return array_unique(Object.getOwnPropertyNames(def.project).map(
-    function(e){return def.project[e].country})).map(
-      function(e){return def.country[e]})}
