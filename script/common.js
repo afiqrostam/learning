e2be63038e1fc6bad0d6895bba583b60d49d97f6
afiq rostam <afiqrostam@hotmail.com>
2018-08-06 00:44:41 +0800

    Update common.js

--- script/common.js
+++ script/common.js
@@ -18,10 +18,9 @@ function get_ready(){
   google.script.run.withSuccessHandler(
     function(e,f){
     if(e.s){
-      init.sp.settings.ranges.forEach(function(a,b){
-        f[a.name]=get_2D(e.res[b].values,a);
-        if(a.name=='bu'){init_bu()}
-        else if(a.name=='project'){init_project()}});
+      init.sp.settings.ranges.forEach(function(a,b){f[a.name]=get_2D(e.res[b].values,a)});
+			init_bu();
+			init_project();
       console.timeEnd(f.f_n);
       delete f.f_n;
       get_alert({id:'feed_success',type:'primary',msg:'load data..success',feed:true});
