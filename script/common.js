87fa8d7951b8f44e3115654d5e71f087f38c7d81
afiq rostam <afiqrostam@hotmail.com>
2018-08-05 18:39:29 +0800

    Update common.js

--- script/common.js
+++ script/common.js
@@ -27,3 +27,29 @@ function get_ready(){
     }}).withUserObject(def).get_batch_data_list({
     sheet_id:init.sp.settings.id,
     sheet_range:init.sp.settings.ranges.map(function(e){return e.sheet})})}
+
+function get_2D(dt){
+  console.time(arguments.callee.name);
+  var lt={};
+  var hd=dt.shift();
+  dt.forEach(function(z){
+    var ip={};
+    z.map(function(y,x){
+			if(y!=""){
+				if(hd[x]=="status"){return ip[hd[x]]=parseInt(y,10)}
+				else{return ip[hd[x]]=y}}});
+        if(ip.id.substr(0,1)=='P'){
+            var root=ip.root.split(',');
+                var find_root=root.filter(function(f){return def.bu[f].country!=undefined});
+                if(find_root.length==0){
+                    while(find_root.length==0){
+                        var new_root=root.map(function(f){if(def.bu[f].root!=undefined){return def.bu[f].root}else{return f}});
+                        if(new_root!=root){root=new_root}else{break}
+                        var find_root=root.filter(function(f){return def.bu[f].country!=undefined})}}
+                if(find_root.length!=0){
+                    find_root=find_root.map(function(f){return def.bu[f].country});
+                    var trim=array_unique(find_root);	
+                    if(trim.length==1){ip.country=trim[0]}}}
+        lt[ip.id]=ip});
+    console.timeEnd(arguments.callee.name);
+    return lt}
