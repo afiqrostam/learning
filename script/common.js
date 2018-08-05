a2f02955a70bfc2f0d72516852c27506f95a166d
afiq rostam <afiqrostam@hotmail.com>
2018-08-05 18:35:45 +0800

    Update common.js

--- script/common.js
+++ script/common.js
@@ -15,7 +15,15 @@ function get_ready(){
     if(e.s){
       init.sp.settings.ranges.map(function(a){return a.name}).forEach(
         function(a,b){f[a]=get_2D(e.res[b].values)});
-      console.timeEnd(f.f_n);delete f.f_n}
+      console.timeEnd(f.f_n);
+      delete f.f_n;
+      get_alert({id:'feed_success',type:'primary',msg:'load data..success',feed:true});
+    }
     else{
-      console.log(e.con);console.timeEnd(f.f_n);delete f.f_n}}).withUserObject(def).get_batch_data_list(
-        {sheet_id:init.sp.settings.id,sheet_range:init.sp.settings.ranges.map(function(e){return e.sheet})})}
+      console.log(e.con);
+      console.timeEnd(f.f_n);
+      delete f.f_n;
+      get_alert({id:'feed_fail',type:'danger',msg:'load data..fail',feed:true});
+    }}).withUserObject(def).get_batch_data_list({
+    sheet_id:init.sp.settings.id,
+    sheet_range:init.sp.settings.ranges.map(function(e){return e.sheet})})}
