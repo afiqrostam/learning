c5681703f84f1b5a9a179e7a638aa0b8658fff56
afiq rostam <afiqrostam@hotmail.com>
2018-08-05 18:28:31 +0800

    Update common.js

--- script/common.js
+++ script/common.js
@@ -1,4 +1,4 @@
-var get_alert=function(e){
+function get_alert(e){
   if(e!=undefined){
     var alert=$('<div id="'+e.id+'" class="alert alert-'+e.type+' alert-dismissible fade show" role="alert">').append(
       $('<strong>').html('!!! ')).append(e.msg);
@@ -6,3 +6,16 @@ var get_alert=function(e){
       $('#feed').append(alert);
       setTimeout(function(){alert.alert('close')},500,alert)}
     else{return alert}}}
+
+function get_ready(){
+  def.f_n=arguments.callee.name;
+  console.time(def.f_n);
+  google.script.run.withSuccessHandler(
+    function(e,f){
+    if(e.s){
+      init.sp.settings.ranges.map(function(a){return a.name}).forEach(
+        function(a,b){f[a]=get_2D(e.res[b].values)});
+      console.timeEnd(f.f_n);delete f.f_n}
+    else{
+      console.log(e.con);console.timeEnd(f.f_n);delete f.f_n}}).withUserObject(def).get_batch_data_list(
+        {sheet_id:init.sp.settings.id,sheet_range:init.sp.settings.ranges.map(function(e){return e.sheet})})}
