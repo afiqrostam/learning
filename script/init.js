53581b37b3c567c5bad557b7c2d39addec3f811e
afiq rostam <afiqrostam@hotmail.com>
2018-08-08 13:48:55 +0800

    Update init.js

--- script/init.js
+++ script/init.js
@@ -2,16 +2,7 @@ var init;
 var def={};
 console.time('init');
 $('#main-loader').modal('show');
-if(window.location.host.includes('googleusercontent')){
-  google.script.run.withSuccessHandler(
-    function(e){
-      if(e.s){
-        console.timeEnd('init');
-        init=e.res;
-        get_ready()}
-      else{
-        console.timeEnd('init');
-        $('#main-loader').modal('hide')}}).get_var('init')}
+if(window.location.host.includes('googleusercontent')){get_init()}
 else{
   console.timeEnd('init');
   $('#main-loader').modal('hide')}
