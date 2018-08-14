527910bb7ed59645ee1cc82a426be5ded7578fde
afiq rostam <afiqrostam@hotmail.com>
2018-08-14 16:38:02 +0800

    Update init.js

--- script/init.js
+++ script/init.js
@@ -1,8 +1,14 @@
 var init={};
 var def={};
+
+function get_nav(){
+  $('.nav-link:contains("Home")').on('click',page_home)}
+
 console.time('init');
 $('#main-loader').modal('show');
-if(window.location.host.includes('googleusercontent')){get_init()}
+if(window.location.host.includes('googleusercontent')){
+  get_init();
+  get_nav()}
 else{
   console.timeEnd('init');
   $('#main-loader').modal('hide')}
