f3177da8eb017691a340e9c2b22077f38d7ca383
afiq rostam <afiqrostam@hotmail.com>
2018-08-14 16:51:41 +0800

    Update common.js

--- script/common.js
+++ script/common.js
@@ -1,15 +1,39 @@
 $('.modal').on('show.bs.modal',function(){$('main').hide()});
 $('.modal').on('hidden.bs.modal',function(){$('main').show()});
 
-function array_unique(a){t=[];a.forEach(function(e){if(t.indexOf(e)==-1){t.push(e)}});return t}
+function array_unique(a){
+	t=[];
+	a.forEach(
+		function(e){
+			if(t.indexOf(e)==-1){t.push(e)}});
+	return t}
+
 function get_init(){
-	def={q:{}};init={};var f=arguments.callee.name;console.time(f);
-	if($('#main-loader').css('display')=='none'){$('#main-loader').modal('show')}
+	def={q:{}};
+	init={};
+	var f=arguments.callee.name;
+	console.time(f);
+	if($('#main-loader').css('display')=='none'){
+		$('#main-loader').modal('show')}
 	google.script.run.withSuccessHandler(
-		function(e,f){if(e.s){init=e.res;console.timeEnd(f);get_ready();get_employees();get_news();
-				google.script.run.withSuccessHandler(function(g){if(g.s){init.us.photo=g.res[0].photo}}).get_users(init.us.email);
-				google.script.url.getLocation(function(g){Object.keys(g).forEach(function(h){init.we.param[h]=g[h]})})}
-			else{console.timeEnd(f);$('#main-loader').modal('hide')}}).withUserObject(f).get_var('init')}
+		function(e,f){
+			if(e.s){
+				init=e.res;
+				console.timeEnd(f);
+				get_ready();
+				get_employees();
+				get_news();
+				google.script.run.withSuccessHandler(
+					function(g){
+						if(g.s){init.us.photo=g.res[0].photo}}).get_users(init.us.email);
+				google.script.url.getLocation(
+					function(g){
+						Object.keys(g).forEach(
+							function(h){
+								init.we.param[h]=g[h]})})}
+			else{
+				console.timeEnd(f);
+				$('#main-loader').modal('hide')}}).withUserObject(f).get_var('init')}
 
 function get_ready(){
   var f=arguments.callee.name;
@@ -84,8 +108,7 @@ function get_news(){
 
 function q_check(){
 	if(Object.keys(def.q).length==0){
-		page_home();
-		display_update(def.news[def.news.length-1],def.news.length-1);
+		$('.nav-link:contains("Home")').trigger('click');
 		$('#main-loader').modal('hide')}}
 
 function get_2D(dt,st){
