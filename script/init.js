834060a8d5924896e818a25dbb58c2bbe6b2eddd
afiq rostam <afiqrostam@hotmail.com>
2018-08-14 16:43:56 +0800

    Update init.js

--- script/init.js
+++ script/init.js
@@ -3,7 +3,10 @@ var def={};
 
 function get_nav(){
   $('.nav-link:contains("Home")').on('click',
-    function(e){e.preventDefault();page_home()})}
+    function(e){
+      e.preventDefault();
+      page_home();
+      display_update(def.news[def.news.length-1],def.news.length-1)})}
 
 console.time('init');
 $('#main-loader').modal('show');
