8ac6240fe3e65aa4a8a95d21723bcabd2e4f7911
afiq rostam <afiqrostam@hotmail.com>
2018-08-16 01:33:15 +0800

    Update demo.js

--- img/demo.js
+++ img/demo.js
@@ -11,7 +11,7 @@
     if(!(img.src||img instanceof HTMLCanvasElement)){content=$('<span>Loading image file failed</span>')}
     else{
       if(!href){
-        href=img.toDataURL('image/jpeg',parseFloat($('input[type="number"]').val()))
+        href=img.toDataURL('image/jpeg',0.9)
         img.className='img-fluid rounded';
         // Check if file type is supported for the dataURL export:
         dataURLStart ='data:'+currentFile.type
