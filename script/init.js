8ecea0c6a05079d19bce3095e09aab0e74960d25
afiq rostam <afiqrostam@hotmail.com>
2018-08-15 13:31:12 +0800

    Update init.js

--- script/init.js
+++ script/init.js
@@ -3,9 +3,9 @@ var def={};
 
 function get_nav(){
   google.script.history.setChangeHandler(function (e) {
-    if(e.hash!=''){
-      if(e.hash=="home"){page_home()}}
-      else if(e.hash=="announcement"){page_announcement()}});
+    if(e.location.hash!=''){
+      if(e.location.hash=="home"){page_home()}}
+      else if(e.location.hash=="announcement"){page_announcement()}});
   $('.nav-link:contains("Home")').on('click',
     function(e){
       e.preventDefault();
